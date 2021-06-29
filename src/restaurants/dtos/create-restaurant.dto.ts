598c2d76b9a83eb5843ace4a0e37a4a65565efc9
Jae Hui Seo <dayjaycs49@gmail.com>
2021-06-28 22:07:32 -0400

    InputType and ArgsType with dto

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,13 +1,13 @@
-import { Field, ObjectType } from "@nestjs/graphql";
+import { ArgsType, Field } from "@nestjs/graphql";
 
 
-@ObjectType()
-export class Restaurant {
+@ArgsType()
+export class CreateRestaurantDto {
   @Field(type => String)
   name: string;
 
   @Field(type => Boolean)
-  isVegan?: boolean;
+  isVegan: boolean;
 
   @Field(type => String)
   address: string;
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
 
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant)
@@ -7,5 +8,12 @@ export class RestaurantResolver {
   @Query(returns => [Restaurant])
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     return [];
+  }
+  @Mutation(returns => Boolean)
+  createRestaurant(
+    @Args() createRestaurantDto: CreateRestaurantDto 
+  ): boolean {
+    console.log(createRestaurantDto);
+    return true;
   }
 }
