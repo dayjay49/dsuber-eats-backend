3fc11c2cdae04ccfab4832982eb071812f5f68d7
Jae Hui Seo <dayjaycs49@gmail.com>
2021-07-01 23:12:13 -0400

    Create Account Mutation part one

--- src/common/entities/core.entity.ts
+++ src/common/entities/core.entity.ts
@@ -1,13 +1,20 @@
-import { CreateDateColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Field } from "@nestjs/graphql";
+import { 
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
 export class CoreEntity {
-
   @PrimaryGeneratedColumn()
+  @Field(type => Number)
   id: number;
 
   @CreateDateColumn()
+  @Field(type => Date)
   createdAt: Date;
 
-  @CreateDateColumn()
+  @UpdateDateColumn()
+  @Field(type => Date)
   updateAt: Date;
 }
