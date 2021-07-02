b66f6f7d93a12a8162b1da878c0d3d4d5698b038
Jae Hui Seo <dayjaycs49@gmail.com>
2021-07-01 21:37:01 -0400

    Update Restaurant Part Two

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
@@ -13,8 +14,13 @@ export class RestaurantService {
   getAll(): Promise<Restaurant[]> {
     return this.restaurants.find();
   }
-  createRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
+  createRestaurant(
+    createRestaurantDto: CreateRestaurantDto,
+  ): Promise<Restaurant> {
     const newRestaurant = this.restaurants.create(createRestaurantDto);
     return this.restaurants.save(newRestaurant);
+  }
+  updateRestaurant({ id, data }: UpdateRestaurantDto) {
+    return this.restaurants.update(id, { ...data });
   }
 }
