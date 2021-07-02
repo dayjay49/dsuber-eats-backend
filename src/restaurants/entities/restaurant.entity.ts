c8f68ca1997ffbe89dc79771eefb60e70e98ff07
Jae Hui Seo <dayjaycs49@gmail.com>
2021-07-01 20:10:29 -0400

    adding Entity decorator to create our entity in our database

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,17 +1,31 @@
 import { Field, ObjectType } from "@nestjs/graphql";
-
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+
+  @PrimaryGeneratedColumn()
+  @Field(type => Number)
+  id: number;
+
   @Field(type => String)
+  @Column()
   name: string;
 
   @Field(type => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field(type => String)
+  @Column()
   address: string;
 
   @Field(type => String)
+  @Column()
   ownersName: string;
+
+  @Field(type => String)
+  @Column()
+  categoryName: string;
 }
