0eef950ef013efa944ff5ce380c39b806c156e8e
Jae Hui Seo <dayjaycs49@gmail.com>
2021-07-05 11:06:47 -0400

    JWT Module Part Three

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,16 +1,20 @@
 import { Inject, Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
 import { JwtModuleOptions } from './jwt.interfaces';
 import { CONFIG_OPTIONS } from './jwt.constants';
+
 
 @Injectable()
 export class JwtService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
-  ) {
-    console.log(options);
-  }
+  ) {}
 
-  hello() {
-    console.log('hello');
+  // use this one if you want to use this module in other projects
+  // sign(payload: object): string {
+  //   return jwt.sign(payload, this.options.privateKey);
+  // }
+  sign(userID: number): string {
+    return jwt.sign({ id: userID }, this.options.privateKey);
   }
 }
