ccfd7f424170a22655c98cbccda215f83da3079d
Jae Hui Seo <dayjaycs49@gmail.com>
2021-07-06 16:01:45 -0400

    Incorporating uuid to create random 'code' for the Verification Entity. Added some lines in createAccount as well as editProfile

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,11 +6,15 @@ import { LoginInput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
 import { JwtService } from "src/jwt/jwt.service";
 import { EditProfileInput } from "./dtos/edit-profile.dto";
+import { Verification } from "./entities/verification.entity";
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User) private readonly users: Repository<User>,
+    @InjectRepository(User)
+    private readonly users: Repository<User>,
+    @InjectRepository(Verification)
+    private readonly verifications: Repository<Verification>,
     private readonly jwtService: JwtService,
   ) {}
 
@@ -26,7 +30,12 @@ export class UsersService {
         // make error
         return { ok: false, error: 'There is a user with that email already'};
       }
-      await this.users.save(this.users.create({ email, password, role }));
+      const newUser = await this.users.save(
+        this.users.create({ email, password, role }),
+      );
+      await this.verifications.save(
+        this.verifications.create({ user: newUser }),
+      );
       return { ok: true };
     } catch (e) {
       // make error
