f27edc55da1c7320ff8a0401b3960b85ff0032f2
Jae Hui Seo <dayjaycs49@gmail.com>
2021-07-17 23:00:21 -0400

    OrderItems entity.. createOrder p2

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { Restaurant } from "src/restaurants/entities/restaurant.entity";
 import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
 import { CreateOrderInput, CreateOrderOutput } from "./dtos/create-order.dto";
@@ -11,13 +12,20 @@ export class OrderService {
   constructor(
     @InjectRepository(Order)
     private readonly orders: Repository<Order>,
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
   ) {}
 
   async createOrder(
     customer: User,
-    createOrderInput: CreateOrderInput,
+    { restaurantId, items }: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
     try {
+      // look for restaurant
+      const restaurant = await this.restaurants.findOne(restaurantId);
+      if (!restaurant) {
+        return { ok: false, error: 'Restaurant not found' };
+      }
       return { ok: true };
     } catch (err) {
       return { ok: false, error: 'Could not create order' };
