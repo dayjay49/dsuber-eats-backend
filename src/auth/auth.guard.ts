18eaaf5b573357ae1547f15ac558b5577cc4cace
Jae Hui Seo <dayjaycs49@gmail.com>
2021-07-19 14:21:24 -0400

    Subscription Authentication p2 -> changed our AuthGuard so our authentication works for both HTTP requests and WS connections

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,7 +1,8 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { GqlExecutionContext } from "@nestjs/graphql";
-import { User } from "src/users/entities/user.entity";
+import { JwtService } from "src/jwt/jwt.service";
+import { UserService } from "src/users/users.service";
 import { AllowedRoles } from "./role.decorator";
 
 
@@ -9,9 +10,13 @@ import { AllowedRoles } from "./role.decorator";
 export class AuthGuard implements CanActivate {
   // if returns true, continue with request
   // if returns false, stop the request
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
 
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     const roles = this.reflector.get<AllowedRoles>(
       'roles',
       context.getHandler(),
@@ -23,17 +28,29 @@ export class AuthGuard implements CanActivate {
     // this HTTP context is different from our GraphQL context
     // line of code below converts the executioncontext of nestjs into gql context
     const gqlContext = GqlExecutionContext.create(context).getContext();
-    console.log(gqlContext.token);
-    // if there is metadata, we expect there to be a user for our private resolver
-    const user: User = gqlContext['user'];
-    if (!user) {
-      // when there is metadata set but NO user... so stop the request
+    const token = gqlContext.token;
+    if (token) {
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        // using the id decoded from the token, look for the corresponding user
+        const { user } = await this.userService.findById(decoded['id']);
+        if (!user) {
+          // when there is metadata set but NO user... so stop the request
+          return false;
+        }
+        gqlContext['user'] = user;
+        if (roles.includes('Any')) {
+          // when there is user and any type of user is allowed for the resolver
+          return true;
+        }
+        return roles.includes(user.role);
+      } else {
+        // when there is a problem with the decoded token
+        return false;
+      }
+    } else {
+      // when there is no token
       return false;
     }
-    if (roles.includes('Any')) {
-      // when there is user and any type of user is allowed for the resolver
-      return true;
-    }
-    return roles.includes(user.role);
   }
 }
